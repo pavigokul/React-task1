f9d9beea73e8741fd4b5780b527a56884062bfe3
pavigokul <pavigokul9788@gmail.com>
2023-06-12 13:55:04 +0530

    react app deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+function Details(props){
+  return(
+    <div>
+
+      <h3 style={{color:'grey'}}>Free</h3>
+      <h1>$0/month</h1>
+      <p>{props.check} Single User</p>
+      <p>{props.check} 5GB Storage</p>
+      <p>{props.check} Unlimited Public Projects</p>
+      <p>{props.check} Community Access</p>
+      <p>{props.cross} Unlimited Private Projects</p>
+      <p>{props.cross} Dedicated Phone Support</p>
+      <p>{props.cross} Free Subdomain</p>
+      <p>{props.cross} Monthly Status Reports</p>
+      <button>Button</button>
+
+
+      <h3 style={{color:'grey'}}>Plus</h3>
+      <h1>$9/month</h1>
+      <h3>{props.check} 5 Users</h3>
+      <p>{props.check} 50GB Storage</p>
+      <p>{props.check} Unlimited Public Projects</p>
+      <p>{props.check} Community Access</p>
+      <p>{props.check} Unlimited Private Projects</p>
+      <p>{props.check} Dedicated Phone Support</p>
+      <p>{props.check} Free Subdomain</p>
+      <p>{props.cross} Monthly Status Reports</p>
+      <button>Button</button>
+
+
+      <h3 style={{color:'grey'}}>Pro</h3>
+      <h1>$49/month</h1>
+      <h3>{props.check} Unlimited Users</h3>
+      <p>{props.check} 150GB Storage</p>
+      <p>{props.check} Unlimited Public Projects</p>
+      <p>{props.check} Community Access</p>
+      <p>{props.check} Unlimited Private Projects</p>
+      <p>{props.check} Dedicated Phone Support</p>
+      <p>{props.check} Unlimited Free Subdomains</p>
+      <p>{props.check} Monthly Status Reports</p>
+      <button>Button</button>
+
+    </div>
+
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        <Details  check='&#10004;' cross='&#10006;'/>
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
